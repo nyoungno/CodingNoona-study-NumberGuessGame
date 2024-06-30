84ea9fc3a98b86810d5d5ed22f8b93ae996c00d0
nyoungno Ko <yghjbm@gmail.com>
2024-06-30 23:25:47 +0900

    pickRandomNum

---
+++ main.js
@@ -0,0 +1,34 @@
+//랜덤번호 지정
+//유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
+//만약에 유저가 랜덤번호는 맞추면, 맞췃습니다.
+//랜덤번호가 < 유저번호 Down!!
+//랜덤번호가 > 유전번호 up!!
+//Rest버튼을 누르면 게임이 리셋된다.
+// 5번의 기회를 다쓰면 게임이 끝난다. (더이상 추측 불가. 버튼이 disavle)
+//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깍지 않는다.
+//유저가 이미 입력한 숫자를 또입 입력하면, 알려준다. 기회를 깍지 않는다.
+
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+
+playButton.addEventListener("click", play);
+
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  console.log("정답", computerNum);
+}
+
+function play() {
+  let userValue = userInput.value;
+  if (userValue < computerNum) {
+    resultArea.textContent = "UP";
+  } else if (userValue > computerNum) {
+    resultArea.textContent = "DOWN";
+  } else {
+    resultArea.textContent = "Success!";
+  }
+}
+
+pickRandomNum();
